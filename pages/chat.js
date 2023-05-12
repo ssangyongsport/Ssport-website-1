34d924c71b748c7a5c2e0841bab9e7d62cff274f
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-12 22:59:05 +0800

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -1,11 +1,8 @@
 import { useEffect, useState } from 'react';
-import { ChatClient } from 'openai';
+import apiClient from '../apiClient';
+import { createChatCompletion } from 'openai';
 
-const chatClient = new ChatClient({
-  apiKey: 'pk-okExSWKrxhHCqHAnLMefSyPOelTPaooGAyukseiRwqPgKoOZ',
-  api_base : 'https://api.pawan.krd/v1m',
-  model: 'gpt-3.5-turbo',
-});
+const chatClient = createChatCompletion({ httpClient: apiClient });
 
 export default function Chat() {
   const [messages, setMessages] = useState([]);
@@ -14,7 +11,9 @@ export default function Chat() {
   useEffect(() => {
     // 處理接收到新訊息的邏輯
     async function handleMessageReceived() {
-      const response = await chatClient.sendMessage(messages);
+      const response = await chatClient.create(
+        messages.map((message) => ({ role: message.role, content: message.content }))
+      );
       const message = response.choices[0].message.content;
       setMessages([...messages, { role: 'assistant', content: message }]);
     }
@@ -39,24 +38,23 @@ export default function Chat() {
       <h1>Chat</h1>
       <div>
         {messages.map((message, index) => (
-          <div key={index} style={{ textAlign: message.role === 'user' ?
-          "left" : "right" }}>
-          {message.role === 'user' ? (
-          <div>
-          <strong>You:</strong> {message.content}
+          <div key={index} style={{ textAlign: message.role === 'user' ? 'left' : 'right' }}>
+            {message.role === 'user' ? (
+              <div>
+                <strong>You:</strong> {message.content}
+              </div>
+            ) : (
+              <div>
+                <strong>ChatGPT:</strong> {message.content}
+              </div>
+            )}
           </div>
-          ) : (
-          <div>
-          <strong>ChatGPT:</strong> {message.content}
-          </div>
-          )}
-          </div>
-          ))}
-          </div>
-          <form onSubmit={handleFormSubmit}>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-          <button type="submit">Send</button>
-          </form>
-          </div>
-          );
-          }
+        ))}
+      </div>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
