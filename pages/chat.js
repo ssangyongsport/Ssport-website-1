503a8e79c8eea86f051de03ba28d7187bcba7c98
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-12 23:08:28 +0800

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import apiClient from '../apiClient';
+import apiClient from './apiClient';
 import openai from 'openai';
 
 const chatClient = new openai.default({
